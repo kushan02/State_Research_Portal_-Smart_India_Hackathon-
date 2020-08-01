8bb6a5ff0a206ed68ab637940e727a100b1822d3
Kushan <10797083+kushan02@users.noreply.github.com>
2020-08-01 07:36:29 +0000

    Yield record functionality

--- data-mining/research-paper-indexer.py
+++ data-mining/research-paper-indexer.py
@@ -2,6 +2,8 @@ import time
 start_time = time.time()
 
 import logging
+import json
+
 from elasticsearch import Elasticsearch
 logging.basicConfig(level=logging.ERROR)
 
@@ -17,3 +19,38 @@ def connect_elasticsearch(url = 'localhost', port = 9200):
 
 
 es = connect_elasticsearch()
+
+
+def yield_record(file_path, encoding="utf8"):
+    line_counter = 0
+    with open(file_path, encoding=encoding) as f:
+        for line in f:
+            single_record = json.loads(line)
+            
+            line_counter = line_counter + 1
+            paper_id = single_record['id']
+            single_record['referenceCount'] = len(single_record['outCitations'])
+            single_record["citationCount"] = len(single_record['inCitations'])
+           
+            single_record['relatedTopics'] = single_record.pop('entities')
+            
+            if line_counter % 10000:
+                print(line_counter)
+#                 single_record["relatedTopics"] = ["kushan", "mehta"]
+            
+            
+            del single_record['id']
+            del single_record['s2Url']
+#             del single_record['inCitations']
+#             del single_record['outCitations']
+            del single_record['sources']
+            del single_record["magId"]
+            for author in single_record['authors']:
+                del author['ids']
+                
+#             print(single_record)
+#             print("")
+            yield paper_id, single_record
+           
+
+    print("Inserted total " + str(line_counter) + " lines")
