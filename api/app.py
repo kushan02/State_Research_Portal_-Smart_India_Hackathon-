ddd81d9f11bba1f5cc9fad2e56f1a008aef652aa
Kushan Mehta <kushan.mehta02@gmail.com>
2020-08-01 09:25:04 -0400

    Add ElasticSearch connection module

--- api/app.py
+++ api/app.py
@@ -14,9 +14,24 @@ app.register_blueprint(authentication_bp_registration)
 app.register_blueprint(authentication_bp_google_oauth)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# def connect_elasticsearch(url='localhost', port=9200):
+def connect_elasticsearch(url='54.237.93.158', port=9200):
+# def connect_elasticsearch(url='9f164f7103f7.ngrok.io', port=443):
+    _es = None
+    _es = Elasticsearch([{'host': url, 'port': port}])
+    # _es = Elasticsearch([{'host': url, 'port': port, 'use_ssl': True}])
+
+    print(_es)
+
+    if _es.ping():
+        print('Connection to ' + url + ':' + str(port) + ' successful')
+    else:
+        print('Ping failed, some error in connection. Check the logs')
+    return _es
+
+
+es = connect_elasticsearch()
+
 
 
 if __name__ == '__main__':
