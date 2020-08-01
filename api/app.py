2af340e38315b37e6c48321425f5d549ca4218a5
Kushan Mehta <kushan.mehta02@gmail.com>
2020-08-01 02:34:03 -0400

    Make flask more secure and liberal the CORS even more for development ease

--- api/app.py
+++ api/app.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.secret_key = "fJm6?7Sb]+4Am0@4'^>E(@*2311_`4?}u]L1Z$^lV=y1Yk-Dkro4@C)*$']D>~sd)np57s>U"
+CORS(app, resources={r'/api/*': {"origins": "*"}})
 
 @app.route('/')
 def hello_world():
