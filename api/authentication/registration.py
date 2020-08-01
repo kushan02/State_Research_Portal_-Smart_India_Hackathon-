c7ad247b5def75cbd7a3e65b85575f3609b77c70
Kushan Mehta <kushan.mehta02@gmail.com>
2020-08-01 11:29:24 -0400

    Added basic user registration backend

--- api/authentication/registration.py
+++ api/authentication/registration.py
@@ -7,3 +7,60 @@ import pymysql
 import re
 
 registration_bp = Blueprint('registration_bp', __name__)
+
+@api_bp.route('/reg', methods=['GET', 'POST'])
+def data_reg():
+    if request.method == 'POST':
+
+        data = (request.json)
+        print(data)
+        if data['fname'] == '' or data['email'] == '' or data['password'] == '' or data['cpass'] == '' or data['city'] == '' or data['institute'] == '':
+            bool_no_null_input = False
+        else:
+            bool_no_null_input = True
+
+        # Email Validation
+        e_mail = data['email']
+
+        regex_email = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        if re.search(regex_email,e_mail):
+            bool_valid_email = True
+        else:
+            bool_valid_email = False
+
+        #password validation
+        pass_hash = bcrypt.generate_password_hash(data['password'])
+        print(pass_hash)
+        pass_hash = pass_hash.decode("utf-8")
+        conf_pass = data['cpass']
+
+        if bcrypt.check_password_hash(pass_hash,conf_pass):
+            bool_valid_pass = True
+        else:
+            bool_valid_pass = False
+
+        print(bool_valid_email)
+        print(bool_valid_pass)
+
+
+        if bool_valid_email == True and bool_valid_pass == True and bool_no_null_input == True:
+            sql = "INSERT INTO users(NAME, EMAIL, PASSWORD, INSTITUTE, CITY, HOMEPAGE, BIO, PHONE_NUMBER) VALUES" + "(" + "'" + (
+                str(data['fname'])) + "'" + "," + "'" + (str(data['email'])) + "'" + "," + "'" + (
+                      str(pass_hash)) + "'" + "," + "'" + (
+                      str(data['institute'])) + "'" + "," + "'" + (str(data['city'])) + "'" + "," + "'" + (
+                      str(data['hp'])) + "'" + "," + "'" + (
+                      str(data['bio'])) + "'" + "," + "'" + (str(data['phone_number'])) + "'" + ")"
+            print(sql)
+            try:
+                cursor.execute(sql)
+                db.commit()
+            except:
+                db.rollback()
+        else:
+            return abort(404,'Wrong/Invalid Data')
+
+
+        cursor.execute("select * from users")
+        data = cursor.fetchall()
+        print(data)
+    return "registered"
