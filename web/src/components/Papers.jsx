6acd81cbbd1e4376802ae8ba89ecda295cb1923a
Megha Sankhlecha <41811242+meghasankhlecha@users.noreply.github.com>
2020-08-01 19:53:07 -0400

    Bug fix of UI exchange

--- web/src/components/Papers.jsx
+++ web/src/components/Papers.jsx
@@ -21,6 +21,76 @@ const {Header, Content, Sider} = Layout;
         </div>
 
 
+
+
+        <div style={{paddingTop:"90px", width:'80%'}}>
+            <div style={{
+                marginLeft:'98%',
+                width:'25%',
+                height:'57px'
+            }}>
+                <div style={{
+                    width: '25%',
+                }}>
+                    <Card title="Author Details" style={{width: 260, height: '70vh', borderRadius: '5px'}}>
+                        <p>Charotar University of Science and Technology </p>
+                        Department of Computer Science and Engineering, Gujarat, India
+                        <br/><br/>
+                        <p><h4>Current position</h4></p>
+                        Student, Devang Patel Institute of Advance Technology , CSE
+                    </Card>
+                </div>
+            </div>
+
+            <div className="profile-main-outer-div" style={{
+                marginTop: '-57px'
+            }}>
+                <div className="papers-inner-div">
+                    <h1>Kushan Mehta<Button size="large" id="contact-button">Contact</Button></h1>
+                    Charotar University of Science and Technology | CHARUSAT
+                    <br/>
+                    BTech - Computer Science & Engineering
+                    <br/><br/>
+                    <h3>Skills</h3>
+                    <Tag style={{padding: '3px', borderRadius: '7px'}}>Data Analytics</Tag>
+                    <Tag style={{padding: '3px', borderRadius: '7px'}}>Machine Learning</Tag>
+                    <Tag style={{padding: '3px', borderRadius: '7px'}}>Data Mining</Tag>
+
+
+                </div>
+
+            </div>
+
+
+            <div className="profile-main-outer-div">
+              <div className="papers-inner-div">
+                  <Row className="summary-row" justify="space-between">
+                      <Col span={8}><h3>1</h3></Col>
+                      <Col span={8}><h3>1,276</h3></Col>
+                      <Col span={8}><h3>7</h3></Col>
+                  </Row>
+                  <Row className="summary-row" justify="space-between">
+
+                      <Col span={8}>Publications</Col>
+                      <Col span={8}>Reads</Col>
+                      <Col span={8}>Citations</Col>
+                  </Row>
+              </div>
+          </div>
+        <div className="papers-outer-div">
+
+        <div className="papers-inner-div">
+
+
+          <h1>Papers</h1>
+          	     <Table columns={columns} dataSource={data1} onChange={onChange} />
+        </div>
+        </div>
+
+        </div>
+
+
+
           <Footer/>
       </React.Fragment>
     );
