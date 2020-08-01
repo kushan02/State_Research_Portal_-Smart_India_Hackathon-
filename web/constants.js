19dd3bc66728333503a529bfa3c3f524c874b13c
Mitanshu Bhatt <46819439+mitanshu610@users.noreply.github.com>
2020-08-01 16:26:43 +0530

    created constants file which includes elasticsearch URL and file name

---
+++ web/constants.js
@@ -0,0 +1,8 @@
+const elasticSearchUrl="http://192.168.0.100:9200/"
+const elasticSearchAppName="research_portal"
+const flaskServerUrl=""
+module.exports = {
+    elasticSearchUrl,
+    elasticSearchAppName,
+    flaskServerUrl,
+  }    
