1705fdfd1c6d3b6e5bd5fcd4e8baeb28f5528010
Megha Sankhlecha <41811242+meghasankhlecha@users.noreply.github.com>
2020-08-01 09:28:49 -0400

    Added backend logic for Login

--- api/authentication/login.py
+++ api/authentication/login.py
@@ -6,3 +6,40 @@ import flask_bcrypt
 import pymysql
 
 login_bp = Blueprint('login_bp', __name__)
+
+
+# CORS(login_bp, resources={r'/api/*': {"origins": "*"}})
+
+
+# login module
+@login_bp.route('/api/login/', methods=['POST'])
+def login_user():
+    if request.method == 'POST':
+        data_login = (request.json)
+        print("Login Request:", data_login)
+        user_email = data_login['user_email']
+        password = data_login['password']
+
+        if not user_email:
+            return 'Email field empty', 400
+        elif not password:
+            return 'Password field empty', 400
+
+        db = pymysql.connect("156.67.222.22", "u133349638_sih_team", "?/lL@YA$", "u133349638_researchportal")
+        cursor = db.cursor()
+
+        params = [user_email]
+        cursor.execute("SELECT user_email, user_password, user_name FROM account WHERE user_email=%s", params)
+
+        db_pass = cursor.fetchone()
+
+        cursor.close()
+        db.close()
+
+        try:
+            if db_pass is not None and db_pass[1] and bool(flask_bcrypt.check_password_hash(db_pass[1], password)):
+                return db_pass[2], 200
+            else:
+                return 'Invalid Credentials', 401
+        except:
+            return 'Invalid Credentials', 401
