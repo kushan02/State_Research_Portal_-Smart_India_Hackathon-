2b4aaef70a943529391df27296a367af2aefc449
Kushan <10797083+kushan02@users.noreply.github.com>
2020-08-02 07:51:39 +0000

    Add backend API for fetching document details

--- api/app.py
+++ api/app.py
@@ -7,16 +7,16 @@ import re
 
 from authentication.login import login_bp as authentication_bp_login
 from authentication.registration import registration_bp as authentication_bp_registration
-from authentication.google_oauth import google_oauth_bp as authentication_bp_google_oauth
+from user_profile.account_details import account_details_bp
 
 app = Flask(__name__)
 app.secret_key = "fJm6?7Sb]+4Am0@4'^>E(@*2311_`4?}u]L1Z$^lV=y1Yk-Dkro4@C)*$']D>~sd)np57s>U"
-CORS(app, resources={r'/api/*': {"origins": "*"}})
+CORS(app, resources={r'/api/*': {"origins": "*"}}, support_credentials=True)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.register_blueprint(authentication_bp_login)
 app.register_blueprint(authentication_bp_registration)
-app.register_blueprint(authentication_bp_google_oauth)
+app.register_blueprint(account_details_bp)
 
 
 # def connect_elasticsearch(url='localhost', port=9200):
