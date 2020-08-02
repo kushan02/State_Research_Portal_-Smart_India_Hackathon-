a73116001c0da4a8d5a1f50b345f51fa04fd7288
Megha Sankhlecha <41811242+meghasankhlecha@users.noreply.github.com>
2020-08-01 20:45:45 -0400

    Add security logs backend API placeholder

--- api/app.py
+++ api/app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request, make_response
+import json
 from flask_cors import CORS, cross_origin
+from elasticsearch import Elasticsearch
+
+import re
 
 from authentication.login import login_bp as authentication_bp_login
 from authentication.registration import registration_bp as authentication_bp_registration
@@ -8,6 +12,7 @@ from authentication.google_oauth import google_oauth_bp as authentication_bp_goo
 app = Flask(__name__)
 app.secret_key = "fJm6?7Sb]+4Am0@4'^>E(@*2311_`4?}u]L1Z$^lV=y1Yk-Dkro4@C)*$']D>~sd)np57s>U"
 CORS(app, resources={r'/api/*': {"origins": "*"}})
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.register_blueprint(authentication_bp_login)
 app.register_blueprint(authentication_bp_registration)
@@ -16,7 +21,7 @@ app.register_blueprint(authentication_bp_google_oauth)
 
 # def connect_elasticsearch(url='localhost', port=9200):
 def connect_elasticsearch(url='54.237.93.158', port=9200):
-# def connect_elasticsearch(url='9f164f7103f7.ngrok.io', port=443):
+    # def connect_elasticsearch(url='9f164f7103f7.ngrok.io', port=443):
     _es = None
     _es = Elasticsearch([{'host': url, 'port': port}])
     # _es = Elasticsearch([{'host': url, 'port': port, 'use_ssl': True}])
@@ -31,6 +36,7 @@ def connect_elasticsearch(url='54.237.93.158', port=9200):
 
 
 es = connect_elasticsearch()
+
 
 @app.route('/api/paper_details/', methods=['GET'])
 @cross_origin()
@@ -49,6 +55,22 @@ def get_paper_details():
     return json.dumps(res)
 
 
+@app.route('/api/security-logs/', methods=['POST'])
+@cross_origin()
+def get_security_logs():
+    data = (request.json)
+    user_id = data["user_id"]
+
+    security_logs = [
+        {"user_id": user_id, "ip": "192.168.2.1", "device": "Android Samsung Galaxy S8",
+         "time": "2020-04-04T01:57:28+0000"},
+        {"user_id": user_id, "ip": "123.456.21.1", "device": "Windows 10 Chrome Browser",
+         "time": "2020-03-04T01:57:28+0000"},
+        {"user_id": user_id, "ip": "111.168.2.1", "device": "Iphone IOS Safari",
+         "time": "2019-04-04T01:57:28+0000"}
+    ]
+
+    return json.dumps(security_logs)
 
 
 if __name__ == '__main__':
